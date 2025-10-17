d0635857ee28aa9a82a131170ec3c694b033be75
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-10-17 18:40:11 +0900

    교재 dom 섹션 예제코드 작성 및 연습

--- study_javascript/day01/js/ex09_03.js
+++ study_javascript/day01/js/ex09_03.js
@@ -1,9 +1,8 @@
 window.onload = function () {
   const button = document.getElementById('button');
   let count = 0;
-
+  
   button.onclick = function addStar() {
-    count++;
-    button.textContent = `버튼 - ${'★'.repeat(count)}`;
+    button.textContent = `버튼 - ${'★'.repeat(++count)}`;
   };
 };
