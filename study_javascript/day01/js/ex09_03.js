dc3c4285557b287274e3b4d595acb851e2c38a39
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-10-17 15:50:57 +0900

    자바스크립트 문제풀이 코드 수정

--- study_javascript/day01/js/ex09_03.js
+++ study_javascript/day01/js/ex09_03.js
@@ -1,6 +1,9 @@
 window.onload = function () {
   const button = document.getElementById('button');
+  let count = 0;
+
   button.onclick = function addStar() {
-    button.textContent += '★';
+    count++;
+    button.textContent = `버튼 - ${'★'.repeat(count)}`;
   };
 };
