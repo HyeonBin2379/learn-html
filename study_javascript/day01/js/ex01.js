334e12f7f4420b8eabbc03babde55e435b35823e
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-10-17 12:39:45 +0900

    자바스크립트 dom 객체 실습1

---
+++ study_javascript/day01/js/ex01.js
@@ -0,0 +1,5 @@
+// window 객체 로드 시 제목 태그에 배경색 적용
+window.onload = function () {
+  document.querySelector('h1').style.backgroundColor = 'green';
+  document.querySelector('h2').style.backgroundColor = 'pink';
+};
