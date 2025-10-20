c459f9f64b36036bee4b29e42c28198971e83c86
Lee Hyeon Bin <hyeonbin2379@gmail.com>
2025-10-20 18:00:03 +0900

    async-await 연습 코드 주석 추가

--- study_http_node/day01/top20/top20-movie-asyncawait.js
+++ study_http_node/day01/top20/top20-movie-asyncawait.js
@@ -2,17 +2,24 @@
 const axios = require('axios');
 
 // async와 await를 사용하여 메서드 체이닝을 사용했던 top20-movie-promise.js의 가독성을 개선
-// async: promise의 then(), catch() 메서드 호출하지 않더라도, 암묵적으로 반환값을 resolve하는 프로미스를 반환
 async function getTop20Movies() {
   const url =
     'https://raw.githubusercontent.com/wapj/jsbackend/main/movieinfo.json';
 
   try {
     // 네트워크로부터 데이터를 수신하므로, await를 사용하여 대기
+    // 즉, await 키워드가 붙은 axios.get(url)가 정상적으로 실행 종료되어야 getTop20Movies()의 나머지 코드 실행 가능
+    // axios.get(url)이 resolve되면 result에 promise 객체를 할당
     const result = await axios.get(url);
-    const { data } = result; // 수신한 result에는 data 프로퍼티가 존재
 
-    if (!data.articleList || data.articleList.size === 0) {
+    // 여기서부터는 동기적으로 실행되는 코드
+    const { data, status } = result; 
+
+    if (status != 200) {
+      throw new Error('request fail!');
+    }
+
+    if (!data && !data.articleList || data.articleList.size === 0) {
       throw new Error('데이터가 없습니다.');
     }
 
@@ -23,6 +30,8 @@ async function getTop20Movies() {
     movieinfos.forEach((movieinfo) =>
       console.log(`[${movieinfo.rank}위] ${movieinfo.title}`)
     );
+
+    // return문을 사용하지 않았으나, async 함수이므로 항상 promise 객체를 반환
   } catch (error) {
     throw new Error(error);
   }
